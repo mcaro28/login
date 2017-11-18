0c69fc15353a751f315b15963efe20ed8818f2f5
mcaro28 <lafemiguel@gmail.com>
2017-11-18 11:10:04 -0500

    maj-system 2017

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+(function () {
+  document.getElementById('toggle').addEventListener('click', function () {
+    document.getElementById('container').classList.toggle('active');
+  });
+  document.getElementById('close').addEventListener('click', function () {
+    document.getElementById('container').classList.toggle('active');
+  });
+})()
+
